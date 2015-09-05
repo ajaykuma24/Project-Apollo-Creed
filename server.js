2243230a9ceabd7ce5fed95a8712f44950f85ee7
ajaykuma24 <ajaykuma24@yahoo.com>
2015-09-05 18:16:38 -0400

    send html

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
+  res.send('index.html')
 }).listen(port);
 
 // var express = require('express');
