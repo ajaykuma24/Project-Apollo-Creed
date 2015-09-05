4bbe8a34376c47a6b330fee73e631cca6cf6d5ef
ajaykuma24 <ajaykuma24@yahoo.com>
2015-09-05 18:51:27 -0400

    logging

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ fs.readFile('./index.html', function (err, html) {
     http.createServer(function(request, response) {  
         response.writeHeader(200, {"Content-Type": "text/html"});  
         response.write(html);  
+        console.log("write")
         response.end();  
     }).listen(process.env.PORT || 80);
 });
