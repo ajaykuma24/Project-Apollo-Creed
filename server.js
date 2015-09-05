89514fc02d398b24f11f9cd81472778b0db61fca
ajaykuma24 <ajaykuma24@yahoo.com>
2015-09-05 18:48:57 -0400

    send html works

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
-var http = require('http')
-var port = process.env.PORT || 80;
-http.createServer(function(req, res) {
-  res.send('index.html')
-}).listen(port);
+var http = require('http'),
+    fs = require('fs');
+
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(process.env.PORT || 80);
+});
 
 // var express = require('express');
 // var bodyParser = require('body-parser')
